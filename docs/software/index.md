4c7ffa02e60220935efe408dc65d3f108fb79a23
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2024-03-05 12:35:42 -0500

    refine software downloader

--- docs/software/index.md
+++ docs/software/index.md
@@ -7,11 +7,15 @@ const software = FileAttachment("software.json").json();
 ```
 
 ```js
-for (const [key, category] of Object.entries(software)) {
-    display(html`<h2>${category.name}</h2>`)
+for (const cat of software) {
+    display(html`<h2>${cat.category_name}</h2>`)
     var items_html = []
-    for (const item of category.items) {
-        items_html.push(html`<div class="card"><a href="${item.link}" target="_blank"><h3>${item.name}</h3>${item.description}</a></div>`)
+    for (const item of cat.items) {
+        if (item.link == null) {
+            items_html.push(html`<div class="card"><h3>${item.emoji} ${item.name}</h3>${item.description}</div>`)
+        } else {
+            items_html.push(html`<div class="card"><a href="${item.link}" target="_blank"><h3>${item.emoji} ${item.name}</h3>${item.description}</a></div>`)
+        }
     }
     display(html`<div class="grid grid-cols-4">${items_html}</div>`)
 }
--- docs/software/software.json.py
+++ docs/software/software.json.py
@@ -1,6 +1,6 @@
 import os
-from dotenv import load_dotenv
 import json
+from dotenv import load_dotenv
 from notion_client import Client
 from notion_client.helpers import collect_paginated_api
 
@@ -11,44 +11,45 @@ results = collect_paginated_api(
     notion.databases.query, database_id="d1cf2e137fe44864bf4dcd5a7a4d1864"
 )
 
-software_by_category = {}
-for item in results:
-    category_obj = item["properties"]["Category"]
-    if category_obj["select"]:
-        category_id = category_obj["select"]["id"]
-        category = category_obj["select"]["name"]
-    else:
-        continue
+software_items = [
+    {
+        "name": (
+            item["properties"]["Name"]["title"][0]["text"]["content"]
+            if item["properties"]["Name"]["title"]
+            else None
+        ),
+        "emoji": item["icon"]["emoji"] if item.get("icon") else None,
+        "category": (
+            item["properties"]["Category"]["select"]["name"]
+            if item["properties"]["Category"].get("select")
+            else None
+        ),
+        "link": item["properties"]["URL"]["url"],
+        "description": (
+            item["properties"]["Description"]["rich_text"][0]["text"]["content"]
+            if item["properties"]["Description"]["rich_text"]
+            else None
+        ),
+        "hosts": [i["name"] for i in item["properties"]["Hosts"]["multi_select"]],
+        "notion_url": item["public_url"],
+    }
+    for item in results
+]
 
-    name = item["properties"]["Name"]["title"][0]["text"]["content"]
-    link = item["properties"]["URL"]["url"]
-
-    description_obj = item["properties"]["Description"]
-    if len(description_obj["rich_text"]):
-        description = description_obj["rich_text"][0]["text"]["content"]
-    else:
-        description = None
-
-    hosts = [i["name"] for i in item["properties"]["Hosts"]["multi_select"]]
-    if len(hosts) == 0:
-        continue
-
-    notion_url = item["public_url"]
-
-    if not category_id in software_by_category:
-        software_by_category.update({category_id: {"name": category, "items": []}})
-
-    software_by_category[category_id]["items"].append(
-        {
-            "name": name,
-            "link": link,
-            "description": description,
-            "hosts": hosts,
-            "notion_url": notion_url,
-        }
-    )
-
-software_by_category = dict(
-    sorted(software_by_category.items(), key=lambda x: x[1]["name"])
+categories = sorted(
+    set(item["category"] for item in software_items if item["category"])
 )
-print(json.dumps(software_by_category, indent=4))
+
+output = [
+    {
+        "category_name": category,
+        "items": [
+            item
+            for item in software_items
+            if item["category"] == category and not "Retired" in item["hosts"]
+        ],
+    }
+    for category in categories
+]
+
+print(json.dumps(output, indent=4))
