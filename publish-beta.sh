48b9a2c72fb1d41747a863fa3319f0b459c7addb
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2024-04-10 13:46:22 -0400

    clean

--- publish-beta.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# build it
-rm -rf dist
-yarn observable build || exit
-
-# copy to beta site
-rm -r /opt/nginx/www/beta.wasabipesto.com/*
-cp -r dist/* /opt/nginx/www/beta.wasabipesto.com/
